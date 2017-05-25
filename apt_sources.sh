92c576bb4f673d558dfa675407250d7f2822d74f
Elisha <kendagor@gmail.com>
2017-05-24 23:21:01 -0400

    apt dotnet sources from yakkety

--- apt_sources.sh
+++ apt_sources.sh
@@ -6,7 +6,7 @@ curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microso
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" | sudo tee /etc/apt/sources.list.d/dotnetdev.list
+echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ yakkety main" | sudo tee /etc/apt/sources.list.d/dotnetdev.list
 sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
 
 sudo add-apt-repository ppa:inkscape.dev/stable

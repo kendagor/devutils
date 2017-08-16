c78797254b56f7447b0f68acf1fbf84df94adbee
Elisha <kendagor@gmail.com>
2017-08-15 22:11:07 -0400

    New dotnet apt sources

--- dotnet.sh
+++ dotnet.sh
@@ -15,15 +15,28 @@ checkfail() {
     fi
 }
 
-echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" | sudo tee /etc/apt/sources.list.d/dotnetdev.list
-checkfail $? "apt sources failed"
+# echo "deb [arch=amd64] https://apt-mo.trafficmanager.net/repos/dotnet-release/ xenial main" | sudo tee /etc/apt/sources.list.d/dotnetdev.list
+# checkfail $? "apt sources failed"
 
-sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
-checkfail $? "apt-key failed"
+# sudo apt-key adv --keyserver apt-mo.trafficmanager.net --recv-keys 417A0893
+# checkfail $? "apt-key failed"
 
-sudo apt update
-checkfail $? "apt update failed"
+# sudo apt update
+# checkfail $? "apt update failed"
 
-sudo apt-get install dotnet-dev-1.0.0-preview2-003121
-checkfail $? "install dotnet-dev-xxx failed"
+# sudo apt-get install dotnet-dev-1.0.0-preview2-003121
+# checkfail $? "install dotnet-dev-xxx failed"
 
+mkdir ~/Downloads/dotnet
+checkfail $? "tmp directory creation failed"
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > ~/Downloads/dotnet/microsoft.gpg
+checkfail $? "Curl failed"
+
+sudo mv ~/Downloads/dotnet/microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
+checkfail $? "Copy microsoft.gpg file failed"
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-zesty-prod zesty main" > /etc/apt/sources.list.d/dotnetdev.list'
+checkfail $? "Create apt dotnetdev.list file failed"
+
+sudo apt-get update

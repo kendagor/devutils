c345bfb998588b136ef65a1cd81d154ad269c75c
Elisha <kendagor@gmail.com>
2024-08-18 11:40:07 -0700

    Fix quotation chars

--- build_gcc.sh
+++ build_gcc.sh
@@ -219,17 +219,17 @@ echo   /usr/local/lib/../lib64
 echo
 echo If you ever happen to want to link against installed libraries
 echo in a given directory, LIBDIR, you must either use libtool, and
-echo specify the full pathname of the library, or use the `-LLIBDIR'
+echo specify the full pathname of the library, or use the '-LLIBDIR'
 echo flag during linking and do at least one of the following:
-echo   - add LIBDIR to the `LD_LIBRARY_PATH' environment variable
+echo   - add LIBDIR to the 'LD_LIBRARY_PATH' environment variable
 echo      during execution
-echo   - add LIBDIR to the `LD_RUN_PATH' environment variable
+echo   - add LIBDIR to the 'LD_RUN_PATH' environment variable
 echo     during linking
-echo   - use the `-Wl,-rpath -Wl,LIBDIR' linker flag
-echo   - have your system administrator add LIBDIR to `/etc/ld.so.conf'
+echo   - use the '-Wl,-rpath -Wl,LIBDIR' linker flag
+echo   - have your system administrator add LIBDIR to '/etc/ld.so.conf'
 echo
 echo See any operating system documentation about shared libraries for
-echo more information, such as the ld(1) and ld.so(8) manual pages.
+echo more information, such as the 'ld(1)' and 'ld.so(8)' manual pages.
 echo ----------------------------------------------------------------------
 
 

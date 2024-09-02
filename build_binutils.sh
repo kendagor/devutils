64e418fbb18bc88eb927d54c490f4be2284007b8
Elisha <kendagor@gmail.com>
2024-09-01 17:51:04 -0700

    Refresh binutils and cmake build scripts

--- build_binutils.sh
+++ build_binutils.sh
@@ -2,7 +2,7 @@
 # Elisha Kendagor
 # Local script for building binutils-gdb
 
-gdb_src=/usr/local/src/binutils-gdb
+gdb_src=/src/binutils-gdb
 build_dir=~/build
 build_binutils_dir=$build_dir/binutils-gdb
 curr_dir=$PWD
@@ -41,16 +41,16 @@ fi
 if [ "$sync" = "true" ]; then
     ls -d $gdb_src    
     if [ $? -gt 0 ]; then
-        sudo git clone git://sourceware.org/git/binutils-gdb.git $gdb_src
+        git clone git://sourceware.org/git/binutils-gdb.git $gdb_src
         checkfail $? "Sync failed"
     else
         cd $gdb_src
-        sudo git pull
+        git pull
         checkfail $? "Sync failed"
     fi
 fi
 
-rm -r $build_binutils_dir
+rm -rf $build_binutils_dir
 mkdir -p $build_binutils_dir
 checkfail $? "Build directory creation failed"
 
@@ -63,7 +63,14 @@ checkfail $? "Unable to copy src links to $build_dir"
 cd $build_binutils_dir
 checkfail $? "Unable to cd $build_binutils_dir"
 
-./configure
+#./configure
+./configure  --enable-gold       \
+             --enable-ld=default \
+             --enable-plugins    \
+             --enable-shared     \
+             --disable-werror    \
+             --enable-64-bit-bfd \
+             --with-system-zlib
 checkfail $? "configure failed"
 
 make -j $(nproc)
@@ -72,10 +79,36 @@ checkfail $? "make failed"
 sudo make install
 checkfail $? "make install failed"
 
+if [ -e /usr/bin/gdb ]; then
+    echo Please note: gdb is also installed at /usr/bin/gdb. /usr/local/bin/gdb is expected to take preference.
+    # sudo mv /usr/local/bin/gdb /usr/local/bin/gdb-dev
+fi
+
+if [ ! -L /usr/local/bin/ld ]; then
+    echo '/usr/local/bin/ld' was expected to be symbolic link to ld.bfm!
+fi
+
+echo  Renaming ld to ld-dev
 sudo mv /usr/local/bin/ld /usr/local/bin/ld-dev
 
-echo Renamed newly built linker to ld-dev in /usr/local/bin/ld-dev
-echo Use update-alternatives to install an option for linker 'ld' at /usr/local/bin/ld-dev
+echo Renaming /usr/local/bin/as to /usr/local/bin/as-dev 
+sudo mv /usr/local/bin/ld.gold /usr/local/bin/ld-dev.gold
+
+echo Renaming /usr/local/bin/ld.bfd to /usr/local/bin/ld-dev.bfd
+sudo mv /usr/local/bin/ld.bfd /usr/local/bin/ld-dev.bfd
+
+echo Renaming /usr/local/bin/as /usr/local/bin/as-dev
+sudo mv /usr/local/bin/as /usr/local/bin/as-dev
+
+echo Helpful commands:
+echo --------------------------------------------------------------------------------------
+echo sudo update-alternatives --install /usr/bin/ld ld /usr/bin/x86_64-linux-gnu-ld 20 \
+--slave /usr/bin/ld.gold ld.gold /usr/bin/x86_64-linux-gnu-ld.gold \
+--slave /usr/bin/as as /usr/bin/x86_64-linux-gnu-as
+echo ---------------------------------------------------------------------------------------
+echo sudo update-alternatives --install /usr/bin/ld ld /usr/local/bin/ld-dev 40 \
+ --slave /usr/bin/ld.gold ld.gold /usr/local/bin/ld-dev.gold \
+ --slave /usr/bin/as as /usr/local/bin/as-dev
 
 cd $curr_dir
 
--- build_cmake.sh
+++ build_cmake.sh
@@ -23,7 +23,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-cmake_src=/usr/local/src/cmake
+cmake_src=/src/cmake
 cmake_build=~/build/cmake
 ret_dir=$PWD
 install=false
